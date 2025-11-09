cd4cbb7960ad682a62d6f20b0c2ba7120e4d6ed6
= <=>
2025-11-09 04:26:06 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+const Page = () => {
+  return (
+    <div>
+      <h2>Welcome to LMS SaaS</h2>
+      <Button>Button</Button>
+    </div>
+    
+  )
+}
+
+export default Page
